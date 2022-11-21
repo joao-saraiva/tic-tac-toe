d83278d3c1870a053cb31e4385ce3b7c6bb7a897
Joao Victor Saraiva <joaovsaraivasousa@gmail.com>
2022-11-21 14:16:36 -0300

    tic_tac_board.rb now can define the winner of a match

--- lib/tic_tac_board.rb
+++ lib/tic_tac_board.rb
@@ -4,16 +4,27 @@
 # choose the next player and validate moves.
 class TicTacBoard
   attr_accessor :board, :players
+  WINNER_SCNEARIOS = [
+    #Row Scenarios
+    [[0,0], [0,1], [0,2]],
+    [[1,0], [1,1], [1,2]],
+    [[2,0], [2,1], [2,2]],
+
+    #Column Scenarios
+    [[0,0], [1,0], [2,0]],
+    [[0,1], [1,1], [2,1]],
+    [[0,2], [1,2], [2,2]],
+
+    #Diagonally Scenarios
+    [[0,0], [1,1], [2,2]],
+    [[0,2], [1,1], [2,0]],
+  ]
 
   def initialize(players)
     raise "Need to be 2 players" if players.compact.size != 2
 
     @players = players.sort_by{ |p| p.first_to_play? ? 0 : 1}
-    @board = [
-      %w[_ _ _],
-      %w[_ _ _],
-      %w[_ _ _]
-    ]
+    start_board
   end
 
   def mark_position(player, coordinates)
@@ -54,14 +65,26 @@ class TicTacBoard
     total_moves_for_char("O")
   end
 
-  def total_moves_for_char(char)
-    total_moves = 0
+  def start_board
+    @board = [
+      %w[_ _ _],
+      %w[_ _ _],
+      %w[_ _ _]
+    ]
+  end
 
-    @board.each do |row|
-      total_moves += row.select{ |column| column == char }.size
-    end
+  alias_method :reset_board, :start_board
 
-    total_moves
+  def x_winner?
+    winner_by_char("X")
+  end
+
+  def o_winner?
+    winner_by_char("O")
+  end
+
+  def draw?
+    !empty_board? && !x_winner? && !o_winner?
   end
 
   private
@@ -77,5 +100,30 @@ class TicTacBoard
 
   def column_out_of_bounds?
     @column > 2 || @column < 0
+  end
+
+  def total_moves_for_char(char)
+    total_moves = 0
+
+    @board.each do |row|
+      total_moves += row.select{ |column| column == char }.size
+    end
+
+    total_moves
+  end
+
+  def winner_by_char(char)
+    WINNER_SCNEARIOS.each do |winner_scenario|
+      x_count = 0
+      winner_scenario.each do |coordinates|
+        row = coordinates.first
+        column = coordinates.last
+        x_count += 1 if @board[row][column] == char
+      end
+
+      return true if x_count == 3
+    end
+
+    false
   end
 end
--- tests/tic_tac_board_test.rb
+++ tests/tic_tac_board_test.rb
@@ -12,6 +12,51 @@ class TicTacBoardTest < MiniTest::Test
     @tic_tac_board = TicTacBoard.new([@player_with_x, @player_with_o])
   end
 
+  def winner_scenarios_by_char(char)
+    {
+      first_line_win: [
+        ["#{char}", "#{char}", "#{char}"],
+        ["_", "_", "_"],
+        ["_", "_", "_"]
+      ],
+      second_line_win: [
+        ["_", "_", "_"],
+        ["#{char}", "#{char}", "#{char}"],
+        ["_", "_", "_"]
+      ],
+      third_line_win: [
+        ["_", "_", "_"],
+        ["_", "_", "_"],
+        ["#{char}", "#{char}", "#{char}"]
+      ],
+      first_column_win: [
+        ["#{char}", "_", "_"],
+        ["#{char}", "_", "_"],
+        ["#{char}", "_", "_"]
+      ],
+      second_coumn_win: [
+        ["_", "#{char}", "_"],
+        ["_", "#{char}", "_"],
+        ["_", "#{char}", "_"]
+      ],
+      third_coumn_win: [
+        ["_", "_", "#{char}"],
+        ["_", "_", "#{char}"],
+        ["_", "_", "#{char}"]
+      ],
+      first_diagonally_win: [
+        ["#{char}", "_", "_"],
+        ["_", "#{char}", "_"],
+        ["_", "_", "#{char}"]
+      ],
+      second_diagonally_win: [
+        ["_", "_", "#{char}"],
+        ["_", "#{char}", "_"],
+        ["#{char}", "_", "_"]
+      ]
+    }
+  end
+
   def test_player_limit_is_2
     assert_raises(StandardError, "Need to be 2 players") {Board.new([@player_with_x, @player_with_o, @player_with_x])}
     assert_raises(StandardError, "Need to be 2 players") {Board.new([])}
@@ -65,5 +110,34 @@ class TicTacBoardTest < MiniTest::Test
 
     @tic_tac_board.mark_position(@player_with_o, [1,1])
     assert_equal 1, @tic_tac_board.total_moves_for_o
+  end
+
+  def test_x_winner?
+    assert_equal false, @tic_tac_board.x_winner?
+
+    winner_scenarios_by_char("X").each do |key, value|
+      @tic_tac_board.board = value
+      assert_equal true, @tic_tac_board.x_winner?
+    end
+  end
+
+  def test_o_winner?
+    assert_equal false, @tic_tac_board.o_winner?
+
+    winner_scenarios_by_char("O").each do |key, value|
+      @tic_tac_board.board = value
+      assert_equal true, @tic_tac_board.o_winner?
+    end
+  end
+
+  def test_draw?
+    assert_equal false, @tic_tac_board.draw?
+    @tic_tac_board.board = [
+      %w[X O X],
+      %w[O X X],
+      %w[O X O]
+      ]
+
+    assert_equal true, @tic_tac_board.draw?
   end
 end
