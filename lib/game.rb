9a6f062723f4173f84ee5b91e3c035ab309c4a97
Joao Victor Saraiva <joaovsaraivasousa@gmail.com>
2022-11-22 09:09:48 -0300

    now we are able to replay the game, loop though player select the correct coordinate

--- lib/game.rb
+++ lib/game.rb
@@ -15,13 +15,30 @@ player2 = Player.new(player1.opposite_marker)
 tic_tac_board = TicTacBoard.new([player1, player2])
 
 while tic_tac_board.on_going?
+  system ("clear")
   puts tic_tac_board.draw_board
 
   puts "Selecy your coordinates Player(#{tic_tac_board.next_player_to_make_a_move.marker}) (Row, Column)"
   coordinates = gets.chomp
   formated_coordinates = coordinates.split(",").map{ |c| c.strip.to_i }
 
+  next unless formated_coordinates.all?{ |f| f.is_a? Integer}
+
   if tic_tac_board.valid_position?(formated_coordinates)
     tic_tac_board.mark_position(tic_tac_board.next_player_to_make_a_move, formated_coordinates)
+  end
+
+  unless tic_tac_board.on_going?
+    system ("clear")
+    puts tic_tac_board.draw_board
+
+    puts "\nX is the winner" if tic_tac_board.x_winner?
+    puts "\nO is the winner" if tic_tac_board.o_winner?
+    puts "\nIt was a draw" if tic_tac_board.draw?
+
+    puts "Do you want to play it again ? (1 - yes, 2 - no)"
+    play_again = gets.chomp.upcase
+
+    tic_tac_board.reset_board if play_again == "1"
   end
 end
